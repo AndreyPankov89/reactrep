51022092d58b1947b9821a29dbdf401518d4be47
hjicvi <apan89@mail.ru>
2019-01-19 19:43:20 +0500

    test

--- lesson2/advance/code/index.js
+++ lesson2/advance/code/index.js
@@ -9,7 +9,7 @@ class MakeBusiness{
 
         let sumSponsors = [...eu, ...rus, 'unexpected sponsor'];
         console.log(`We have a business. Owner: ${ owner }, director: ${director}. Our budget: ${cash}. And our employers: ${emp}`);
-        console.log('And we have a sponsors: ');
+        console.log('And we have a sponsors:  ');
         console.log(...sumSponsors);
         console.log(`Note. Be careful with ${eu[0]}. It's a huge risk.`);
     }
